7489b0436b99002b9965cc9a4d444b0517eb9cd6
roshnikri03 <roshnikumari90403@gmail.com>
2026-02-05 23:21:57 +0530

    initialize main.py file

---
+++ main.py
@@ -0,0 +1,87 @@
+from fastapi import FastAPI,HTTPException,Query,Depends,status,Response
+from typing import Optional,List
+import mysql.connector
+import time
+from database import Base,engine,SessionLocal
+from models import User
+#from . import models,schemas
+from sqlalchemy.orm import Session
+from pydantic import ValidationError
+from sqlalchemy import text
+
+import sys
+sys.path.append(r'D:/API ENV/main.py')
+import models , schemas , crud
+from crud import create_user, read_users, read_user, update_user, delete_user
+models.Base.metadata.create_all(bind=engine)
+from sqlalchemy import func
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+while True:
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            port="3306",
+            user="root",
+            password="naJL9!2d",
+            database="Digital_Wallet"
+        )
+        print("Connected")
+        break
+
+
+    except Exception as error:
+        print("Connection Failed")
+        print("Error", error)
+        time.sleep(2)
+
+
+mycursor = mydb.cursor()
+
+
+@app.post("/users/", response_model=schemas.UserBase)
+def create_user_api(user: schemas.UserCreate, db: Session = Depends(get_db)):
+     db_user = create_user(db=db, user=user)
+     return schemas.UserBase.from_orm(db_user)
+
+# get all users
+@app.get("/users/", response_model=List[schemas.User])
+def read_users_api(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    query = "SELECT * FROM Users"
+    mycursor.execute(query, {"limit": limit, "skip": skip})
+    users = mycursor.fetchall()
+    return [
+        schemas.User(
+            id=user[0],
+            username=user[1],
+            password=user[2],
+            complete_name=user[3],
+            email_address=user[4]
+        )
+        for user in users
+    ]
+
+
+# get a specific user by id
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user_api(user_id: int, db: Session = Depends(get_db)):
+    query = "SELECT * FROM Users WHERE id = %s"
+    mycursor.execute(query, (user_id,))
+    user = mycursor.fetchone()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return schemas.User(
+        id=user[0],
+        username=user[1],
+        password=user[2],
+        complete_name=user[3],
+        email_address=user[4]
+    )
