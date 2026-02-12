9e27735c24663d0aa8639052796daed165d8b795
roshnikri03 <roshnikumari90403@gmail.com>
2026-02-12 22:36:33 +0530

    feat: Implement member update and delete functionalities, and enhance member retrieval in crud.py

--- crud.py
+++ crud.py
@@ -70,3 +70,27 @@ def create_member(db: Session, member: schemas.MemberCreate):
 
 def get_member(db: Session, member_id: int):
     return db.query(Member).filter(Member.Member_id == member_id).first()
+
+def get_members(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(Member).offset(skip).limit(limit).all()
+
+
+def update_member(db: Session, member_id: int, member_update: MemberUpdate):
+    member = db.query(Member).filter(Member.Member_id == member_id).first()
+    if not member:
+        raise HTTPException(status_code=404, detail="Member not found")
+    update_data = member_update.dict(exclude_unset=True)
+    db.query(Member).filter(Member.Member_id == member_id).update(update_data)
+    db.commit()
+    db.refresh(member)
+    return  {"message":"Member updated Successfully"}
+
+
+
+def delete_member(db: Session, member_id: int):
+    member = db.query(models.Member).filter(models.Member.Member_id == member_id).first()
+    if member:
+        db.delete(member)
+        db.commit()
+        return  {"message":"Member deleted Successfully"}
+    
